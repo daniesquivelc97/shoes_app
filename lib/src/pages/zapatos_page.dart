4f9ca62775be8ec6a1bcbb0d38b4867c2b108967
daniesquivelc97 <dani_esquivelc@hotmail.com>
2022-02-23 15:11:36 -0500

    Página 1 de zapatos

--- lib/src/pages/zapatos_page.dart
+++ lib/src/pages/zapatos_page.dart
@@ -8,11 +8,24 @@ class ZapatoPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          CustomAppBar(texto: 'For you'),
-          SizedBox(
-            height: 20,
+          const CustomAppBar(texto: 'For you'),
+          const SizedBox(height: 20),
+          Expanded(
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: const [
+                  ZapatoSizePreview(),
+                  ZapatoDescription(
+                    titulo: 'Nike Air Max 720',
+                    descripcion:
+                        "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
+                  ),
+                ],
+              ),
+            ),
           ),
-          ZapatoSizePreview(),
+          AgregarCarritoBoton(monto: 180.0),
         ],
       ),
     );
--- lib/src/widgets/custom_widgets.dart
+++ lib/src/widgets/custom_widgets.dart
@@ -1,3 +1,5 @@
 export 'package:shoes_app/src/widgets/custom_appbar.dart';
-
 export 'package:shoes_app/src/pages/zapatos_page.dart';
+export 'package:shoes_app/src/widgets/zapato_descrip.dart';
+export 'package:shoes_app/src/widgets/agregar_carrito.dart';
+export 'package:shoes_app/src/widgets/boton_naranja.dart';
